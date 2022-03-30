41fe3250b5a35ad052a6f8b26b178dbb9a824bca
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-03-30 18:54:55 +0530

    feat: added videolisting page with api call

--- FitVid/src/Pages/VideoListing/VideosContainer/VideosContainer.js
+++ FitVid/src/Pages/VideoListing/VideosContainer/VideosContainer.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Video from "./Video";
 import "../videolisting.css";
 import { videos } from "../../../Temp/video";
+import { fetchVideos } from "../../../Util/fetch-videos";
 
 const VideosContainer = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(async () => {
+    const { data, success, message } = await fetchVideos();
+
+    if (success) {
+      setVideos(data);
+    } else {
+      console.log(message);
+    }
+  }, []);
+
   return (
     <div className="videos-container">
       {videos.map((video) => {
