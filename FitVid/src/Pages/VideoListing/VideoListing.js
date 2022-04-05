3bf04b49cb39c370e99abb97d114d5ffd8236768
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-04-06 01:14:04 +0530

    implemented loader on video page

--- FitVid/src/Pages/VideoListing/VideoListing.js
+++ FitVid/src/Pages/VideoListing/VideoListing.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChipsContainer from "../../Components/ChipsContainer/ChipsContainer";
 import Footer from "../../Components/Footer/Footer";
+import Loader from "../../Components/Loader/Loader";
 import Navbar from "../../Components/NavBar/Navbar";
 import SideBar from "../../Components/SideBar/SideBar";
 import VideosContainer from "./VideosContainer/VideosContainer";
 
 const VideoListing = () => {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+  return loading ? (
+    <Loader />
+  ) : (
     <div>
       <Navbar />
       <div className="content-section">
